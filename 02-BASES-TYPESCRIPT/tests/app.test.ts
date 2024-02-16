510783baa896ada9699e329c457f86edda3ea33f
Sebastian Moreno E <92068938+Cvaz007@users.noreply.github.com>
2024-02-16 13:28:32 -0500

    test example

--- 02-BASES-TYPESCRIPT/tests/app.test.ts
+++ 02-BASES-TYPESCRIPT/tests/app.test.ts
@@ -1,14 +1,14 @@
 describe("Test in the App File", () => {
-  test("should be true", () => {
+  test("should be 30", () => {
     //1. Arrange
-
+    const num1 = 10;
+    const num2 = 20;
 
     //2.Act
-
+    const result = num1 + num2;
 
     //3. Assert
+    expect(result).toBe(30);
 
-
-    
   });
 });
